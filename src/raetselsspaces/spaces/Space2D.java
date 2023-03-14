3511af33ef4cf4de4eb621adb0ec6c0cd7c3192c
epicary22 <epicary22@gmail.com>
2023-03-14 18:33:35 +0000

    Begin spaces and terminals

---
+++ src/raetselsspaces/spaces/Space2D.java
@@ -0,0 +1,16 @@
+package raetselsspaces.spaces;
+
+// Usable types
+import raetselsspaces.Vector2D;
+import raetselsspaces.Point2D;
+import raetselsspaces.Scalar;
+
+// import raetselsspaces.NDimensionalSpace;
+
+public class Space2D // extends Space
+{
+	public Space2D()
+	{
+		// This space can hold types applicable to it; this is a Cartesian Plane.
+	}
+}
---
+++ src/raetselsspaces/terminals/Terminal2D.java
@@ -0,0 +1,16 @@
+package raetselsspaces.terminals;
+
+import raetselsspaces.NDimensionalTerminal;
+import raetselsspaces.Vector2D;
+import raetselsspaces.Point2D;
+import raetselsspaces.Scalar;
+// import raetselsspaces.Space2D;
+
+public class Terminal2D // extends Terminal
+{
+	public Terminal2D()
+	{
+		// Visual terminal interface with a back-end Space2D object.
+	}
+}
+
