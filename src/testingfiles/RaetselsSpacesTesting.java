2b41a74b0eb1ddfa9a943129eb70130bf96a541f
epicary22 <epicary22@gmail.com>
2023-05-08 22:14:50 +0000

    bug, to be fixed in intellij

--- src/testingfiles/RaetselsSpacesTesting.java
+++ src/testingfiles/RaetselsSpacesTesting.java
@@ -1,11 +1,13 @@
 package testingfiles;
 
-import raetselsspaces.vectors.*;
+import testingfiles.raetselsspaces.vectors.Vector3D;
 
 public class RaetselsSpacesTesting
 {
 	public static void main(String[] args)
 	{
 		System.out.println(Vector3D.I.multiply(4).add(Vector3D.J.multiply(5).add(Vector3D.K.multiply(2.54))).rename("myvec").direction());
+		System.out.println(new Vector3D("myvec", 4, 5, 2.54).direction());
+		// TODO SCALAR PRECISION TEST
 	}
 }
